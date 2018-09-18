acac194a937628886ece3aba038308d5aa6d3ac9
Joost van Uitert <j.vanuitert@rubicon.nl>
2018-09-18 21:55:13 +0200

    Open a word document and convert the contents to a string

--- Web/WebApplication/Controllers/HomeController.cs
+++ Web/WebApplication/Controllers/HomeController.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using WebApplication.Models;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using System.Text;
 
 namespace WebApplication.Controllers
 {
@@ -38,6 +44,45 @@ namespace WebApplication.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ImportContract(IFormFile file)
+        {
+            if(file == null || file.Length == 0)
+            {
+                ViewBag.Message = "Not a valid file.";
+                return RedirectToAction("Index");
+            }
+
+            string fileHash;
+            string filePath = Path.GetTempFileName();
+
+            try
+            {
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyToAsync(stream).Wait();
+
+                    using (WordprocessingDocument document = WordprocessingDocument.Open(stream, false))
+                    {
+                        var body = document.MainDocumentPart.Document.Body;
+                        var stringBuilder = new StringBuilder();
+                        foreach (var item in body)
+                        {
+                            stringBuilder.Append(item.InnerText);
+                        }
+                        return Content(stringBuilder.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Not a valid file.";
+                return View();
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
